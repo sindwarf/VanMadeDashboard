deb56f1cc3d4ffbb23a3acb3f3c37dede736beb9
sindwarf <sindorfsolutions@gmail.com>
2023-01-10 12:01:39 -0800

    added tailwind

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello I Am App</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello I Am App
+    </h1>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './client/src/*.{html,jsx}',
+    './client/src/**/*.{html,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
